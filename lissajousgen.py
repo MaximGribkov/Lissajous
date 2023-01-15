bd03bd3c2be6a2a1832ffa4d47e862d56604f526
Ivanov Ivan <no_mail@email.com>
2023-01-15 17:23:18 +0300

    added phase shift and removed delay

--- lissajousgen.py
+++ lissajousgen.py
@@ -1,31 +1,28 @@
 import numpy as np
-import time
 
 
-class lissajous_figure:
+class LissajousFigure:
     """
     Фигуры Лиссажу.
     Задаётся набором точек с координатами x и y.
     """
-    def __init__(self, x_array, y_array):
 
-        self.x_arr=x_array
-        self.y_arr=y_array
+    def __init__(self, x_array, y_array):
+        self.x_arr = x_array
+        self.y_arr = y_array
 
 
 class LissajousGenerator:
     """
     Генерирует фигуры Лиссажу с заданными параметрами
     """
+
     def __init__(self, resolution=20):
+        """
+        Убрал задержку
+        """
+        self._resolution = None
         self.set_resolution(resolution)
-        
-        # Эта задержка эмулирует процедуру инициализации следующей версии генератора.
-        # Задержка будет убрана после обновления.
-        # Пока не трогать.
-        # P.S. В новом генераторе задержка будет только при инициализации.
-        # Фигуры будут генерироваться так же быстро, как и сейчас.
-        time.sleep(1)
 
     def set_resolution(self, resolution):
         """
@@ -33,11 +30,12 @@ class LissajousGenerator:
         """
         self._resolution = resolution
 
-    def generate_figure(self, freq_x, freq_y):
+    def generate_figure(self, freq_x, freq_y, shift):
         """
         Генерирует фигуру (массивы x и y координат точек) с заданными частотами.
+        Добавил сдвиг фаз, которого не хватало по формуле
         """
         t = np.linspace(0, 2 * np.pi, self._resolution)
-        x = np.sin(freq_x * t)
+        x = np.sin(freq_x * t + shift)
         y = np.cos(freq_y * t)
-        return lissajous_figure(x, y)
+        return LissajousFigure(x, y)
